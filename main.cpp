c3a3170c796bc5b5211dd645aad08bd8b9ccacd1
Me <planetaryaxis93@gmail.com>
2026-02-22 17:26:02 -0800

    Final

--- main.cpp
+++ main.cpp
@@ -16,65 +16,6 @@ struct CarDealer {
     }
 };
 
-void inputCars(Cars *);
-void displayCars(Cars *);
-
-int main() {
-    CarDealer *carlisting = new CarDealer[NR_cars];
-    
-    for (int i = 0; i < NR_cars; i++)
-        inputStudent(&carlisting[i]);
-    
-    for (int i = 0; i < cars; i++)
-        displayStudent(&roster[i]);
-    
-    return 0;
-}
-
-void inputCars(Cars * sptr) {
-    static int nrStu = 1;
-    cout << "Put business name: " << nrStu << ":\n";
-    cout << "Name: ";
-    getline(cin, sptr->business);
-    cout << "business that is being accessed: ";
-    cin >> sptr->business;
-    sptr -> cars = new std::string[NR_cars];
-    for (int i = 0; i < NR_GRADES; i++) {
-        cout << "input car: " << i + 1;
-        cin >> sptr->grades[i];
-    }
-    cin.ignore();
-    cout << endl << endl;
-    nrStu++;
-}
-
-void displayCars(Cars * MeMePtr) {
-    cout << "Car Dealer info:\n";
-    cout << "Business: " << sptr->business << endl;
-    cout << "Business Address: " << sptr->BusinessAddress << endl;
-    for (int i = 0; i < NR_cars; i++)
-        cout << "Car: " << i + 1 << ": " 
-             << sptr->cars[i] << endl;
-    cout << endl << endl;
-}
-#include <iostream>
-
-// car listing service
-
-const int NR_cars = 3;
-
-struct CarDealer {
-    std::string business;
-    std::string BusinessAddress;
-    std::string * cars;
-
-    ~CarDealer() {
-        if (cars) 
-            delete [] cars;
-        cars = nullptr;
-    }
-};
-
 void inputCars(CarDealer *);
 void displayCars(CarDealer *);
 
@@ -92,11 +33,10 @@ int main() {
 
 void inputCars(CarDealer * sptr) {
     static int nrStu = 1;
-    std::cout << "Put business name: " << nrStu << ":\n";
-    std::cout << "Name: ";
+    std::cout << "Put business name: " << ":\n";
     getline(std::cin, sptr->business);
-    std::cout << "business that is being accessed: ";
-    std::cin >> sptr->business;
+    std::cout << "business Address: ";
+    std::cin >> sptr->BusinessAddress;
     sptr -> cars = new std::string[NR_cars];
     for (int i = 0; i < NR_cars; i++) {
         std::cout << "input car: " << i + 1;
@@ -109,10 +49,11 @@ void inputCars(CarDealer * sptr) {
 
 void displayCars(CarDealer * MeMePtr) {
     std::cout << "Car Dealer info:\n";
-    std::cout << "Business: " << MeMePtr->cars << std::endl;
+    std::cout << "Business: " << MeMePtr->business << std::endl;
     std::cout << "Business Address: " << MeMePtr->BusinessAddress << std::endl;
-    for (int i = 0; i < NR_cars; i++)
-        std::cout << "Car: " << i + 1 << ": " 
-             << MeMePtr->cars[i] << std::endl;
+    for (int i = 0; i < NR_cars; i++){
+        std::cout << "Car: " << i + 1 << ": "; 
+        std::cout << MeMePtr->cars[i] << std::endl;
     std::cout << std::endl << std::endl;
+}
 }
